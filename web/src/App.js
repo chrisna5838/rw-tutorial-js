cbd359463a4d41cb5e96ef43b1d99f0837960fbd
kiwi-admin <kiwi@kiwi-admins-MacBook-Pro.local>
2022-08-26 14:45:17 +0700

    ganti ke supabase auth

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import { AuthProvider } from '@redwoodjs/auth'
+import { createClient } from '@supabase/supabase-js'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
@@ -8,10 +9,22 @@ import Routes from 'src/Routes'
 import './scaffold.css'
 import './index.css'
 
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+
+const supabaseClient = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+)
+
+console.log(supabaseUrl)
+console.log(supabaseKey)
+console.log(supabaseClient)
+
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <AuthProvider type="dbAuth">
+      <AuthProvider client={supabaseClient} type="supabase">
         <RedwoodApolloProvider>
           <Routes />
         </RedwoodApolloProvider>
